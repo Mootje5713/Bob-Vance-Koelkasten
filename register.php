bd1b14d85bdd20a5412b96e912a8e442f5bb1afa
Mohammed baallach <2113080@talnet.nl>
2022-09-13 15:24:20 +0200

    hello

--- register.php
+++ register.php
@@ -1,10 +1,14 @@
 <?php
 include "connection.php";
-if (isset($_POST['username']) && ($_POST['password'])) {
+if (isset($_POST['first_name']) && ($_POST['last_name']) && ($_POST['username'])
+    && ($_POST['email']) && ($_POST['password'])) {
+    $first_name =  $_POST['first_name'];
+    $last_name =  $_POST['last_name'];
     $username =  $_POST['username'];
+    $email =  $_POST['email'];
     $password =  $_POST['password'];
     $user = "INSERT INTO `user`(first_name, last_name, username, email, password)
-        VALUES ('$username', '$password')";
+        VALUES ('$first_name', '$last_name', '$username', '$email', '$password')";
     header("location: login.php");
     if ($conn->query($user) === FALSE) {
         echo "error" . $user . "<br />" . $conn->error;
@@ -20,7 +24,13 @@ include "header.php";
 ?>
 
 <form action="" method="POST">
+    firstname <input type="text" name="first_name" id="first_name" required>
+    <br>
+    lastname <input type="text" name="last_name" id="last_name" required>
+    <br>
     username <input type="text" name="username" id="username" required>
+    <br>
+    email <input type="text" name="email" id="email" required>
     <br>
     password <input type="password" name="wachtwoord" id="wachtwoord" required>
     <br>
